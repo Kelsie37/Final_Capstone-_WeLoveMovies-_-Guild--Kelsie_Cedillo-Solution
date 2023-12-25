d1083f339313621361266d656ae37351ab565631
Kelsie Cedillo <kelsie@indeed.com>
2023-12-25 15:46:15 -0600

    WeLoveMovies capstone

---
+++ src/movies/movies.controller.js
@@ -0,0 +1,28 @@
+const service = require("./movies.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+async function movieExists(request, response, next) {
+  const { movieId } = request.params;
+  const movie = await service.read(movieId);
+  if (movie) {
+    response.locals.movie = movie;
+    return next();
+  }
+  return next({ status: 404, message: `Movie cannot be found.` });
+}
+
+async function read(request, response) {
+  response.json({ data: response.locals.movie });
+}
+
+async function list(request, response) {
+  const is_showing = request.query.is_showing === 'true'; // assuming is_showing is a query parameter
+  const list = await service.list(is_showing);
+  response.json({ data: list });
+}
+
+module.exports = {
+  list: [asyncErrorBoundary(list)],
+  read: [asyncErrorBoundary(movieExists), read],
+  movieExists,
+};
